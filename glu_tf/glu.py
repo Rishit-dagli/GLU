92def56165b32a0b4404c1a3d157c93ec2184722
Rishit Dagli <rishit.dagli@gmail.com>
2022-05-04 06:08:31 +0000

    Add SeGLU

--- glu_tf/glu.py
+++ glu_tf/glu.py
@@ -64,3 +64,16 @@ class SwiGLU(tf.keras.layers.Layer):
         gate = tf.keras.activations.swish(gate)
         x = tf.multiply(out, gate)
         return x
+
+class SeGLU(tf.keras.layers.Layer):
+    def __init__(self, bias=True, dim=-1, **kwargs):
+        super(SeGLU, self).__init__(**kwargs)
+        self.bias = bias
+        self.dim = dim
+        self.dense = tf.keras.layers.Dense(2, use_bias=bias)
+    
+    def call(self, x):
+        out, gate = tf.split(x, num_split=2, axis=self.dim)
+        gate = tf.keras.activations.selu(gate)
+        x = tf.multiply(out, gate)
+        return x
