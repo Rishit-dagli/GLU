852749b2cb78cb4bc9126255583b4485a7f6c26f
Rishit Dagli <rishit.dagli@gmail.com>
2022-05-04 05:55:04 +0000

    Reformat

--- glu_tf/glu.py
+++ glu_tf/glu.py
@@ -1,16 +1,15 @@
 import tensorflow as tf
 
 
-
 class GLU(tf.keras.layers.Layer):
-    def __init__(self, bias = True, dim=-1, **kwargs):
+    def __init__(self, bias=True, dim=-1, **kwargs):
         super(GLU, self).__init__(**kwargs)
         self.bias = bias
         self.dim = dim
         self.dense = tf.keras.layers.Dense(2, use_bias=bias)
-    
+
     def call(self, x):
-        out, gate = tf.split(x, 2, axis=self.dim)
-        gate =  tf.sigmoid(gate)
+        out, gate = tf.split(x, num_split=2, axis=self.dim)
+        gate = tf.sigmoid(gate)
         x = tf.multiply(out, gate)
         return x
