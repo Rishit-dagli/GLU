4ebe66dd2146e9c8a9e9b9458223adbfb203fb1d
Rishit Dagli <rishit.dagli@gmail.com>
2022-05-04 05:58:01 +0000

    Add GLU Bilinear

--- glu_tf/glu.py
+++ glu_tf/glu.py
@@ -13,3 +13,15 @@ class GLU(tf.keras.layers.Layer):
         gate = tf.sigmoid(gate)
         x = tf.multiply(out, gate)
         return x
+
+class GLUBilinear(tf.keras.layers.Layer):
+    def __init__(self, bias=True, dim=-1, **kwargs):
+        super(GLUBilinear, self).__init__(**kwargs)
+        self.bias = bias
+        self.dim = dim
+        self.dense = tf.keras.layers.Dense(2, use_bias=bias)
+
+    def call(self, x):
+        out, gate = tf.split(x, num_split=2, axis=self.dim)
+        x = tf.multiply(out, gate)
+        return x
