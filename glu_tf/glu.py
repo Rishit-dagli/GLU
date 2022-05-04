4e789e741baba008d1b2727ce8a43da9fe6fdd56
Rishit Dagli <rishit.dagli@gmail.com>
2022-05-04 05:50:41 +0000

    Update GLU implementation

--- glu_tf/glu.py
+++ glu_tf/glu.py
@@ -1,15 +1,16 @@
 import tensorflow as tf
 
 
-class GLU(tf.keras.layers.Layer):
-    def __init__(self, size, bias=True, **kwargs):
-        super(GLU, self).__init__(**kwargs)
-        self.size = size
-        self.dense1 = tf.keras.layers.Dense(size, use_bias=bias)
-        self.dense2 = tf.keras.layers.Dense(size, use_bias=bias)
 
+class GLU(tf.keras.layers.Layer):
+    def __init__(self, bias = True, dim=-1, **kwargs):
+        super(GLU, self).__init__(**kwargs)
+        self.bias = bias
+        self.dim = dim
+        self.dense = tf.keras.layers.Dense(2, use_bias=bias)
+    
     def call(self, x):
-        chunk1 = self.dense1(x)
-        chunk2 = tf.sigmoid(x) * self.dense2(x)
-        x = tf.multiply(chunk1, chunk2)
+        out, gate = tf.split(x, 2, axis=self.dim)
+        gate =  tf.sigmoid(gate)
+        x = tf.multiply(out, gate)
         return x
